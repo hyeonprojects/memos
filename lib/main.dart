313a5df47ae65fa3708231ab313d5480a62dd229
dalsa <dalsa@jaranda.kr>
2022-05-21 03:27:21 +0900

    create: 웹뷰를 만들어서 테스트 진행

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Memo Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    home: MyHomePage(),
+    );
+  }
 }
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebView(
+          initialUrl: 'https://parent-dev.jaranda.kr',
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
+  }
+}
+
